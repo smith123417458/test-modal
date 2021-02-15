ae9c6f4312db5ef75b81ae981f28bacfe5c25d92
盛建雄 <a0952987429@yahoo.com.tw>
2021-02-15 23:07:05 +0800

    table用bootstap過濾

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,68 +1,44 @@
 <template>
-  <div>
-    <div class="text-right mt-4 d-flex justify-content-center">
-      <button class="btn btn-primary text-white " @click="openCouponModal(true)">
-        建立新的---優惠券
-      </button>
-    </div>
-     <button class="btn btn-outline-primary btn-sm"
-       @click="openCouponModal()"
-       >編輯</button>
-   <button class="btn btn-outline-danger btn-sm" @click="openDelCouponModal(item)">刪除</button>
-    <div class="modal fade" id="couponModal" tabindex="-1" role="dialog"
-      aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">優惠券</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div class="form-group">
-              <label for="title">標題</label>
-              <input type="text" class="form-control" id="title" v-model="tempCoupon.title"
-                placeholder="請輸入標題">
-            </div>
-            <div class="form-group">
-              <label for="coupon_code">優惠碼</label>
-              <input type="text" class="form-control" id="coupon_code" v-model="tempCoupon.code"
-                placeholder="請輸入優惠碼">
-            </div>
-            <div class="form-group">
-              <label for="due_date">到期日</label>
-              <input type="date" class="form-control" id="due_date"
-                v-model="due_date">
-            </div>
-            <div class="form-group">
-              <label for="price">折扣百分比</label>
-              <input type="number" class="form-control" id="price"
-                v-model="tempCoupon.percent" placeholder="請輸入折扣百分比">
-            </div>
-            <div class="form-group">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox"
-                  :true-value="1"
-                  :false-value="0"
-                  v-model="tempCoupon.is_enabled" id="is_enabled">
-                <label class="form-check-label" for="is_enabled">
-                  是否啟用
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary text-white"
-              @click="updateCoupon">更新優惠券</button>
-          </div>
-        </div>
-      </div>
-    </div>
+ <div class="container">
+  <h2>Filterable Table</h2>
+  <p>Type something in the input field to search the table for first names, last names or emails:</p>  
+  <input class="form-control" id="myInput" type="text" placeholder="Search..">
+  <br>
+  <table class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody id="myTable">
+      <tr>
+        <td>盛建雄</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@mail.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@greatstuff.com</td>
+      </tr>
+      <tr>
+        <td>Anja</td>
+        <td>Ravendale</td>
+        <td>a_r@test.com</td>
+      </tr>
+    </tbody>
+  </table>
+  
+  <p>Note that we start the search in tbody, to prevent filtering the table headers.</p>
+</div>
 
-   
-  </div>
 </template>
 
 <script>
@@ -70,26 +46,19 @@ import $ from 'jquery';
 export default {
   data() {
     return {
-      coupons: {},
-      tempCoupon: {
-        title: '',
-        is_enabled: 0,
-        percent: 100,
-        due_date: 0,
-        code: '',
-      },
-      due_date: new Date(),
-      isNew: false,
+     
     };
   },
- 
-  methods: {
-    openCouponModal() {
-      $('#couponModal').modal('show'); 
-    },
 
-  },
-  
-};
+created(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+ 
+}
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
  <div class="d-flex justify-content-center">  
-<button type="button" class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#exampleModal">
-<!-- <button type="button" class="btn btn-primary " @click="openCouponModal(true)">-->
- 按了沒有反應 
-</button> 
+   <h1>{{ans}}</h1>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+  
+
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  按下會彈出視窗
+</button>
+
+
 
 
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -11,19 +21,27 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
       <div class="modal-body">
-        ...
+        <h3>{{ans}}</h3>
+        <br>
+        <br>
+        <br>
+        <button @click="add">加號</button>
+        <button @click="minus">減號</button>
+        <button @click="clear">全部清除</button>
       </div>
+      
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
 </div>
-
 
 </div>
 </template>
@@ -35,12 +53,32 @@
 
 export default {
   name: 'Home',
- 
-  // methods: {
-  //   openCouponModal() {
-  //     $('#exampleModal').modal('show'); 
-  //   },
+ data(){
+  
+  return{  
+        ans:'',
+        test:'測試文字',
+         }
+          
+ },
+ methods:{
+   add(){
+     if(this.ans.includes("加號")){
+     return
+     }else  {this.ans = this.ans.concat("加號")}
+   },
+   minus(){
+     
+    this.ans = this.ans.concat("減號")
+   },
 
-  // },
+    clear(){
+    this.ans =""
+   },
+
+ }
+
+ 
+ 
 }
 </script>
